9849ff94003e924ecb0998c990eb41cfb545bf96
Your Name <you@example.com>
2019-06-07 12:21:14 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, render_template, request 
+import pyotp
+app = Flask(__name__)
+text= ""
+@app.route('/',methods = ['POST', 'GET'])
+def index():
+    return render_template('index.html', otp=text)
+@app.route('/verify',methods=['POST', 'GET'])
+def verify():
+    if request.method == 'POST':
+        key = request.form['key']
+        try:
+            return pyotp.TOTP(str(key)).now()
+        except:
+            return "0" 
+if __name__ == '__main__':
+   app.run(debug = True)
+  
